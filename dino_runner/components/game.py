35197ef92500319fd6e36ca5bc96ea06cbb726bc
Omar Gonzales <Omar.Gonzales@fundacion-jala.org>
2023-03-31 09:37:18 -0400

    class1: task

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,6 +1,6 @@
 import pygame
 
-from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
+from dino_runner.utils.constants import BG, CLOUD, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
 
 
 class Game:
@@ -14,6 +14,8 @@ class Game:
         self.game_speed = 20
         self.x_pos_bg = 0
         self.y_pos_bg = 380
+        self.x_pos_cloud = SCREEN_WIDTH
+        self.y_pos_cloud = 100
 
     def run(self):
         # Game loop: events - update - draw
@@ -21,7 +23,7 @@ class Game:
         while self.playing:
             self.events()
             self.update()
-            self.draw()
+            self.draw() 
         pygame.quit()
 
     def events(self):
@@ -36,6 +38,7 @@ class Game:
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
+        self.draw_cloud()
         pygame.display.update()
         pygame.display.flip()
 
@@ -47,3 +50,12 @@ class Game:
             self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
             self.x_pos_bg = 0
         self.x_pos_bg -= self.game_speed
+
+    def draw_cloud(self):
+        cloud_width = CLOUD.get_width()
+        self.screen.blit(CLOUD, (self.x_pos_cloud, self.y_pos_cloud))
+        self.screen.blit(CLOUD, (cloud_width + self.x_pos_cloud, self.y_pos_cloud))
+        if self.x_pos_cloud + cloud_width <= 0:
+            self.screen.blit(CLOUD,(cloud_width + self.x_pos_cloud, self.y_pos_cloud))
+            self.x_pos_cloud = SCREEN_WIDTH
+        self.x_pos_cloud -= self.game_speed
